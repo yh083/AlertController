f710009538807f3df162e9fe43200f8d77b0de3e
liguiyang <sdfa>
2018-11-07 08:23:33 +0800

    Initial Commit

---
+++ AlertController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertController
+//
+//  Created by liguiyang on 2018/11/7.
+//  Copyright © 2018年 liguiyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
